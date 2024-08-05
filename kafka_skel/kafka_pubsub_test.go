fd4b5d88fdd56946f3e7bcaeab1c1b6ffbb72553
Cristian Cretan <cristi.cretan0@gmail.com>
2024-08-05 20:50:08 +0300

    feat(kafka): add basic implementation of each system
    - Add basic implementation of invetory system, billing system and shipping system
    
    - Modify tests accordingly
    
    Issue: NA-0000

--- kafka_skel/kafka_pubsub_test.go
+++ kafka_skel/kafka_pubsub_test.go
@@ -7,50 +7,110 @@
 package main
 
 import (
+	"sync"
 	"testing"
 )
 
+var inventorySystem *InventorySystem
+var billingSystem *BillingSystem
+var shippingSystem *ShippingSystem
+
 func TestOrderPlacedNotification(t *testing.T) {
 	brokers := []string{"localhost:9092"}
+	inventorySystem = newInventorySystem()
+	inventorySystem.Items = make([]Item, 5)
+	inventorySystem.Items[0] = Item{
+		ID:  1,
+		Qty: 5,
+	}
+	inventorySystem.Items[1] = Item{
+		ID:  2,
+		Qty: 5,
+	}
+	inventorySystem.Items[2] = Item{
+		ID:  3,
+		Qty: 5,
+	}
+	inventorySystem.Items[3] = Item{
+		ID:  4,
+		Qty: 5,
+	}
+	inventorySystem.Items[4] = Item{
+		ID:  5,
+		Qty: 5,
+	}
+	billingSystem = newBillingSystem(1000)
+	shippingSystem = newShippingSystem()
+	shippingSystem.Customers = make([]Customer, 3)
+	shippingSystem.Customers[0] = Customer{
+		ID:      501,
+		Address: "Nowhere is your first customer",
+	}
+	shippingSystem.Customers[1] = Customer{
+		ID:      502,
+		Address: "Somewhere is your first customer",
+	}
+	shippingSystem.Customers[2] = Customer{
+		ID:      503,
+		Address: "Some address is your first customer",
+	}
 	producer, err := NewProducer(brokers)
 	if err != nil {
 		t.Fatalf("Failed to create producer: %v", err)
 	}
 
-	consumer, err := NewConsumer(brokers)
-	if err != nil {
-		t.Fatalf("Failed to create consumer: %v", err)
-	}
-
-	topic := "order-topic"
 	order := Order{
-		orderID:    1001,
-		customerID: 501,
-		items:      make([]Item, 2),
+		OrderID:    1001,
+		CustomerID: 501,
+		Items:      make([]Item, 2),
 	}
-	order.items[0] = Item{
+	order.Items[0] = Item{
 		1,
-		2,
+		-2,
 	}
-	order.items[1] = Item{
+	order.Items[1] = Item{
 		2,
-		1,
+		-1,
 	}
 	// Produce an "Order Placed" message
-	err = producer.Send(topic, "Order Placed: "+order.String())
+	err = placeOrder(producer, order)
 	if err != nil {
-		t.Fatalf("Failed to send message: %v", err)
+		t.Fatalf("Failed to place order: %v", err)
+		return
 	}
+	var wg sync.WaitGroup
 
 	// Implement verification logic for expected consumer behavior
 	// This typically involves setting up consumers for each subsystem and validating received messages.
 	// Note: Due to the asynchronous nature of Kafka, synchronization mechanisms or mocking may be required.
 	go func() {
-		err = consumer.Receive(topic)
+		wg.Add(1)
+		err := inventorySystem.Consumer.Receive(invetorySystemTopic)
 		if err != nil {
-			t.Errorf("Failed to receive message: %v", err)
+			t.Errorf("Failed to consume message on invetory system: %v", err)
 			return
 		}
+		wg.Done()
 	}()
 
+	go func() {
+		wg.Add(1)
+		err = billingSystem.Consumer.Receive(billingSystemTopic)
+		if err != nil {
+			t.Errorf("Failed to consume message on billing system: %v", err)
+			return
+		}
+		wg.Done()
+	}()
+
+	go func() {
+		wg.Add(1)
+		err = shippingSystem.Consumer.Receive(shippingSystemTopic)
+		if err != nil {
+			t.Errorf("Failed to consume message on shipping system: %v", err)
+			return
+		}
+		wg.Done()
+	}()
+	wg.Wait()
 }
--- kafka_skel/main.go
+++ kafka_skel/main.go
@@ -7,36 +7,104 @@ import (
 )
 
 const (
-	invetorySystem = "invetory-system"
-	billingSystem  = "billing-system"
-	shippingSystem = "shipping-system"
+	invetorySystemTopic = "invetory-system"
+	billingSystemTopic  = "billing-system"
+	shippingSystemTopic = "shipping-system"
 )
 
 type Item struct {
 	ID  int
-	qty int
+	Qty int
 }
 
 type Order struct {
-	orderID    int
-	customerID int
-	items      []Item
+	OrderID    int
+	CustomerID int
+	Items      []Item
 }
 
 func (o Order) String() string {
 	itemsStr := "["
-	for i := 0; i < len(o.items)-1; i++ {
-		itemsStr += fmt.Sprintf("{id: %d, qty: %d}, ", o.items[i].ID, o.items[i].qty)
+	for i := 0; i < len(o.Items)-1; i++ {
+		itemsStr += fmt.Sprintf("{id: %d, qty: %d}, ", o.Items[i].ID, o.Items[i].Qty)
 	}
-	itemsStr += fmt.Sprintf("{id: %d, qty: %d}]", o.items[len(o.items)-1].ID, o.items[len(o.items)-1].qty)
-	return fmt.Sprintf("{orderID: %d, customerID: %d, items: %s}", o.orderID, o.customerID, itemsStr)
+	itemsStr += fmt.Sprintf("{id: %d, qty: %d}]", o.Items[len(o.Items)-1].ID, o.Items[len(o.Items)-1].Qty)
+	return fmt.Sprintf("{orderID: %d, customerID: %d, items: %s}", o.OrderID, o.CustomerID, itemsStr)
 }
 
 var brokers = []string{"localhost:9092"}
 var topic = "test-topic"
 
-func placeOrder(order Order) {
+type InventorySystem struct {
+	Items    []Item
+	Consumer *Consumer
+}
 
+func newInventorySystem() *InventorySystem {
+	consumer, err := NewConsumer(brokers)
+	if err != nil {
+		log.Fatalf("Error creating invetory system: %v", err)
+	}
+	return &InventorySystem{
+		Items:    []Item{},
+		Consumer: consumer,
+	}
+}
+
+type BillingSystem struct {
+	Balance  float64
+	Consumer *Consumer
+}
+
+func newBillingSystem(balance float64) *BillingSystem {
+	consumer, err := NewConsumer(brokers)
+	if err != nil {
+		log.Fatalf("Error creating billing system: %v", err)
+	}
+	return &BillingSystem{
+		Balance:  balance,
+		Consumer: consumer,
+	}
+}
+
+type Customer struct {
+	ID      int
+	Address string
+}
+
+type ShippingSystem struct {
+	Customers []Customer
+	Consumer  *Consumer
+}
+
+func newShippingSystem() *ShippingSystem {
+	consumer, err := NewConsumer(brokers)
+	if err != nil {
+		log.Fatalf("Error creating shipping system: %v", err)
+	}
+	return &ShippingSystem{
+		Customers: []Customer{},
+		Consumer:  consumer,
+	}
+}
+
+func placeOrder(producer *Producer, order Order) error {
+	err := producer.Send(invetorySystemTopic, order.String())
+	if err != nil {
+		log.Fatalf("Error placing order in invetory system: %v", err)
+		return err
+	}
+	err = producer.Send(billingSystemTopic, order.String())
+	if err != nil {
+		log.Fatalf("Error placing order in billing system: %v", err)
+		return err
+	}
+	err = producer.Send(shippingSystemTopic, order.String())
+	if err != nil {
+		log.Fatalf("Error placing order in shipping system: %v", err)
+		return err
+	}
+	return nil
 }
 
 func main() {
